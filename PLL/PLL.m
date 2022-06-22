b27e041103057291b70ca594aede02ffa9ee5e3f
yfffzhang <zyf1125@zju.edu.cn>
2022-06-22 21:28:14 +0100

    update

---
+++ PLL/PLL.m
@@ -0,0 +1,12 @@
+function dx=PLL(t,x)        
+
+global Ug;
+id=1;
+iq=0;
+
+
+dx = [ ;
+      ];
+      
+
+
---
+++ PLL/PLL_para.m
@@ -0,0 +1,45 @@
+% This function generates the paramters at workspace for simulink model.
+
+% Author(s): Yitong Li
+
+%%
+clear all
+close all
+clc
+
+%%
+t0 = 2;
+dt = 0.2;
+
+%% Fundamental parameters
+Fs = 100;             % (kHz)
+Fs = Fs*1e3;
+Ts = 1/Fs;
+
+%% Base values
+Wbase = 2*pi*50;    % (rad/s)
+Vbase = 1;
+Sbase = 1;
+Ibase = Sbase/Vbase;
+Zbase = Vbase/Ibase;
+Ybase = 1/Zbase;
+
+%% AC filter parameters
+Lf = 0.05;
+Cf = 0.02;
+Lc = 1e-9;
+
+
+
+%% Grid-following inverter
+% PLL
+w_pll = 15 *2*pi;   % (rad/s)
+w_tau = 1e3 *2*pi;  % (rad/s)
+
+% Current loop
+w_i_GFL = 1000 *2*pi;    % (rad/s)
+
+% Rated line impedance
+X_rated_GFL = 0.4;
+R_rated_GFL = X_rated_GFL/5;
+
